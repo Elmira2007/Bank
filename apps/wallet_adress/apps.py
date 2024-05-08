440010807897e16b3b384f96c0d8716b300be648
Elmira2007 <moldosakovaelmira@gmail.com>
2024-05-08 12:36:00 +0600

    there were some changes

--- apps/wallet_adress/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class WalletAdressConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.wallet_adress'
--- core/settings.py
+++ core/settings.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
     
     # apps
     'apps.users',
-    'apps.wallet_adress',
+    
     
 ]
 
@@ -137,3 +137,9 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+}
